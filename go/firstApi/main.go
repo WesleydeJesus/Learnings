33f6ceb0b711eee6510ff8739f7196fbf109e2c9
Wesey <wesley@meuboomerang.com>
2022-01-02 18:55:05 -0300

    remove hardcoded data

--- go/firstApi/main.go
+++ go/firstApi/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"Learnings/go/firstApi/database"
-	"Learnings/go/firstApi/models"
 	"Learnings/go/firstApi/routes"
 	"fmt"
 )
 
 
 func main() {
-	models.Personalities = []models.Personality{
-		{Id: 1, Name: "Wesley", Description: "Opa1"},
-		{Id: 2, Name: "Gabi", Description: "opa2" },
-	} 
 	database.ConnectDB()
 	fmt.Println("Inicializando server")
 	routes.HandleRequest()
