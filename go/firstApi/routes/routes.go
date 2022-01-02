b3ee81e9fd0627f693c713e719b4d34efe6f04cb
Wesey <wesley@meuboomerang.com>
2022-01-02 19:05:02 -0300

    fix: Resolve CORS problem

--- go/firstApi/routes/routes.go
+++ go/firstApi/routes/routes.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -19,5 +20,5 @@ func HandleRequest(){
 	router.HandleFunc("/api/personalities/{id}", controllers.DeletePersonality).Methods("Delete")
 	router.HandleFunc("/api/personalities/{id}", controllers.EditPersonality).Methods("Put")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(router)))
 }
