a55884df04745eace4cc66b0e5117486e3182559
Wesey <wesley@meuboomerang.com>
2022-01-02 16:21:48 -0300

    feat: add pkg, interface and finish course

---
+++ go/contas/contaCorrente.go
@@ -0,0 +1,43 @@
+package contas
+
+import "Learnings/go/clientes"
+
+type ContaCorrente struct{
+	Titular clientes.Titular
+	NAgencia, NConta int
+	saldo float64
+}
+
+
+func (c *ContaCorrente) Saque (valor float64) string {
+	saque := valor > 0 && valor <= c.saldo
+	if saque {
+		c.saldo -= valor
+		return "Saque Efetuado \n " 
+	} else {
+		return "saldo insuficiente \n "
+	} 
+}
+
+func (c *ContaCorrente) Deposito (valor float64) (string, float64) {
+	if valor > 0 {
+		c.saldo += valor
+		return "Valor depositado com sucesso", c.saldo
+	}else {
+		return "Algo deu errado", c.saldo
+	}
+}
+
+func (c *ContaCorrente) Transferencia (valor float64, conta *ContaCorrente) (bool){
+	if valor < c.saldo && valor > 0 {
+		c.saldo -= valor
+		conta.Deposito(valor)
+		return true;
+	}else {
+		return false
+	}
+}
+
+func (c *ContaCorrente) ObterSaldo () float64 {
+	return c.saldo
+}
---
+++ go/contas/contaPoupanca.go
@@ -0,0 +1,33 @@
+package contas
+
+import "Learnings/go/clientes"
+
+
+type ContaPoupanca struct {
+	Titular clientes.Titular
+	NAgencia, NConta, Operacao int
+	saldo float64
+}
+
+func (c *ContaPoupanca) Saque (valor float64) string {
+	saque := valor > 0 && valor <= c.saldo
+	if saque {
+		c.saldo -= valor
+		return "Saque Efetuado \n " 
+	} else {
+		return "saldo insuficiente \n "
+	} 
+}
+
+func (c *ContaPoupanca) Deposito (valor float64) (string, float64) {
+	if valor > 0 {
+		c.saldo += valor
+		return "Valor depositado com sucesso", c.saldo
+	}else {
+		return "Algo deu errado", c.saldo
+	}
+}
+
+func (c *ContaPoupanca) ObterSaldo () float64 {
+	return c.saldo
+}
